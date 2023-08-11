3bebd3518e17f0b52bdc1a9b38bac0b64723f1d9
Marco Balletta <ballettamarco1@gmail.com>
2023-08-11 17:18:53 +0200

    import 3d models with animations

--- Assets/Scripts/Enemies/AnimationManagerEnemy.cs
+++ Assets/Scripts/Enemies/AnimationManagerEnemy.cs
@@ -6,11 +6,13 @@ public class AnimationManagerEnemy : AnimationManager<Enemy>
 {
 
     private EventManagerEnemy eventManager;
+    private Animator animator;
 
     protected override void Awake()
     {
         base.Awake();
         eventManager = GetComponent<EventManagerEnemy>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     protected override void OnEnable()
@@ -21,5 +23,25 @@ public class AnimationManagerEnemy : AnimationManager<Enemy>
     protected override void OnDisable()
     {
         base.OnDisable();
+    }
+
+    private void StartWalking()
+    {
+        animator.SetBool(Constants.ANIMATION_MOVEMENT, true);
+    }
+
+    private void StopWalking()
+    {
+        animator.SetBool(Constants.ANIMATION_MOVEMENT, false);
+    }
+
+    private void Death()
+    {
+        animator.SetTrigger(Constants.ANIMATION_DEATH);
+    }
+
+    private void Shoot()
+    {
+        animator.SetTrigger(Constants.ANIMATION_SHOOT);
     }
 }
